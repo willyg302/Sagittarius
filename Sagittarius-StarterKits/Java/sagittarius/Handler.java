dfacc0a5ec407d8ad33c279339c3400b81aa5fa3
William Gaul <willyg302@gmail.com>
2013-07-28 13:10:57 -1000

    Faux delegate support through inner classes

---
+++ Sagittarius-StarterKits/Java/sagittarius/Handler.java
@@ -0,0 +1,10 @@
+/**
+ * Sagittarius - Java Starter Kit
+ * Copyright 2013 WillyG Productions
+ * @Authors: William Gaul
+ */
+package sagittarius;
+
+public abstract class Handler {
+    public abstract void invoke();
+}
